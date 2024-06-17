f7cdcfec5fc59c2f31e6a12c667cf0c54aba5cab
AdityaRHavaldar <aditya29101@gmail.com>
2024-06-17 11:21:07 +0530

    tailwind installed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,42 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
+// import Header from "./Header";
+import Body from "./Body";
+// import Footer from "./Footer";
+// import About from "./About";
+import Error from "./Error";
+// import Contact from "./contact";
+
+const appRouter = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App/>,
+      errorElement: <Error/>,
+      children: [
+        {
+          path: "/",
+          element: <Body/>
+        },
+        // {
+        //   path: "/about",
+        //   element: <About/>
+        // },
+        // {
+        //   path: "/contact",
+        //   element: <Contact/>
+        // },
+      ]
+    }
+  ]
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {appRouter} />
   </React.StrictMode>
 );
 
